e7a876c048785f32aacd2d69c76148c27ea750ce
Arijit-123 <1603074@kiit.ac.in>
2024-03-27 10:37:21 +0530

    validation and ui changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,51 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+
+import { validation } from '../utils/Validation';
 function Login() {
 
   const [isisignin, setIssignin]=useState(true);
+const [errormessage,setErrormessage]=useState();
+
+
 const togglesigninForm=()=>{
 setIssignin(!isisignin);
 }
 
+const email=useRef(null);
+const password=useRef(null);
+
+function handleclickbutton(){
+  
+const message=validation(email.current.value,password.current.value);
+
+  console.log("message",message);
+  setErrormessage(message);
+}
+
+
+
+console.log("useref",email);
+
+
   return (
     <div>
     <Header/>
-   <div className='absolute'>
+   <div className='absolute w-full h-full'>
     <img  src='https://www.pcworld.com/wp-content/uploads/2024/03/Netflix-Hintergrund.jpg?quality=50&strip=all' alt='logo'></img>
    </div>
 
-   <form className='absolute w-3/12 p-12  bg-black mx-auto my-36 right-0 left-0 text text-white bg-opacity-80'>
-   <h1>{isisignin?"Sign in":"Sign up"}</h1>
-    <input className='p-4 my-4 w-full rounded-lg bg-gray-700' placeholder='email@gmail.com'></input>
-    <input className='p-4 my-4 w-full rounded-lg bg-gray-700' placeholder='password'></input>
-    <button className='text my-4 text-white bg-red-700 w-full p-2 rounded-lg'>Submit</button>
+   <form onSubmit={(e)=>e.preventDefault()} className='absolute w-4/12 p-12  bg-black mx-auto my-36 right-0 left-0 text text-white bg-opacity-80'>
+   <h1 className='text text-xl font font-bold'>{isisignin?"Sign In":"Sign Up"}</h1>
+   {!isisignin? <input className='p-4 my-4 w-full rounded-lg bg-gray-700' placeholder='Full Name'></input>:""}
+  
+  
+    <input className='p-4 my-4 w-full rounded-lg bg-gray-700'  ref={email} placeholder='email@gmail.com'></input>
+    <input className='p-4 my-4 w-full rounded-lg bg-gray-700' ref={password} placeholder='password'></input>
+    <p className='text-red-500 text-lg py-2'>{errormessage}</p>
+    <button className='text my-4 text-white bg-red-700 w-full p-2 rounded-lg' onClick={handleclickbutton}>{isisignin?"Sign in": "Sign up"}</button>
 
-    <p className='text text-white' onClick={togglesigninForm}>{isisignin?"Already registered! sign in now":"New to Netflix sign up now"}</p>
+    <p className='text text-white' onClick={togglesigninForm}>{isisignin?"New to Netflix sign up now":"Already registered! sign in now"}</p>
    </form>
    
     </div>
